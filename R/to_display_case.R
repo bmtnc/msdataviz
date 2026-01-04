ead45374e5fa57d3120354391e36b8655b961b18
Barry Matanic <barrymatanic@gmail.com>
2026-01-03 21:09:08 -0500

    feat: string formatting functions

---
+++ R/to_display_case.R
@@ -0,0 +1,12 @@
+#' Convert snake_case to Display Case
+#'
+#' Converts strings like "specialty_chemicals" to "Specialty Chemicals".
+#'
+#' @param x Character vector
+#' @return Character vector in Title Case
+#' @keywords internal
+to_display_case <- function(x) {
+  x %>%
+    stringr::str_replace_all("_", " ") %>%
+    tools::toTitleCase()
+}
---
+++ R/to_snake_case.R
@@ -0,0 +1,15 @@
+#' Convert String to snake_case
+#'
+#' Converts strings like "SPECIALTY CHEMICALS" or "Specialty Chemicals" to "specialty_chemicals".
+#'
+#' @param x Character vector
+#' @return Character vector in snake_case
+#' @keywords internal
+to_snake_case <- function(x) {
+  x %>%
+    tolower() %>%
+    stringr::str_replace_all("\\s+", "_") %>%
+    stringr::str_replace_all("[^a-z0-9_]", "_") %>%
+    stringr::str_replace_all("_+", "_") %>%
+    stringr::str_remove("^_|_$")
+}
